c2fb612478307e34c64d1f4d3f0d80ca7d46fd46
wasabbi25 <anastasia2005@yahoo.com>
2026-02-26 20:28:37 +0100

    Small changes

---
+++ src/01_load_and_clean.py
@@ -0,0 +1,5 @@
+# Step 1. Defining the file paths
+
+raw_path = "qualitative-quantitative-group2/data/raw/Data_Set_S1.txt"
+clean_path = "qualitative-quantitative-group2/data/clean/clean_data.csv"
+
